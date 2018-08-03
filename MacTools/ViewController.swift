a7c71a59e67140aecdfd0cfc4b8be1e4e3a93886
xingl <935858549@qq.com>
2018-08-03 17:31:41 +0800

    init project

---
+++ MacTools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacTools
+//
+//  Created by xingl on 2018/8/3.
+//  Copyright © 2018年 xingl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
